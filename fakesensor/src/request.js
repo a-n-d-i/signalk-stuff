104c999c25d52b942da1645a99f7f8a321b773f1
Andi <a-n-d-i@users.noreply.github.com>
2017-04-25 18:59:42 +0200

    fixed grafana stuff

--- fakesensor/src/request.js
+++ fakesensor/src/request.js
@@ -11,6 +11,14 @@ var options = {
                     {
                         "path": "environment.inside.temperature",
                         "value": 206.15
+                    },
+                    {
+                        "path": "environment.inside.humidity",
+                        "value": 206.15
+                    },
+                    {
+                        "path": "sensors.sensor1vcc",
+                        "value": 206.15
                     }
                 ],
                 "source": {
@@ -27,6 +35,10 @@ var options = {
 //console.log("BLA")
 var sendData = function () {
     options.body.updates[0].values[0].value = Math.floor((Math.random() * 20) + 273);
+    options.body.updates[0].values[1].value = Math.floor((Math.random() * 100) + 0);
+    options.body.updates[0].values[2].value = Math.random() * 1.75 + 3.25;
+
+
     rp(options)
         .then(function (response) {
             console.log(response);
